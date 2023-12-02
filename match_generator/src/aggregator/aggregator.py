02c7769994cf56523ec2c391957f4a9e2b983d21
borahkaushik <kaushik@bungeetech.com>
2023-12-02 11:17:12 +0530

    data merging start

--- match_generator/src/aggregator/aggregator.py
+++ match_generator/src/aggregator/aggregator.py
@@ -1,39 +1,86 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import ArrayType
-class Aggregator:
-    def __init__(self,glue_context) -> None:
-        self.glue_context=glue_context
+# class Aggregator:
+#     def __init__(self,glue_context) -> None:
+#         self.glue_context=glue_context
         
     
-    def aggregate(self,delta_match_df,existing_match_df):
-        existing_match_df.cache()
-        delta_match_df.cache()
-        existing_match_df.show()
-        delta_match_df.show()
-        print('---------------count of existing b4 explode is',existing_match_df.count())
+#     def aggregate(self,delta_match_df,existing_match_df):
+#         existing_match_df.cache()
+#         delta_match_df.cache()
+#         existing_match_df.show()
+#         delta_match_df.show()
+#         print('---------------count of existing b4 explode is',existing_match_df.count())
         
-        existing_match_df=existing_match_df.withColumn("type_score_list", explode(existing_match_df.type_score_list))
-        existing_match_df=existing_match_df.select(col('*'),col('type_score_list')[0].alias('source_type'),col('type_score_list')[1].alias('score'))
+#         existing_match_df=existing_match_df.withColumn("type_score_list", explode(existing_match_df.type_score_list))
+#         existing_match_df=existing_match_df.select(col('*'),col('type_score_list')[0].alias('source_type'),col('type_score_list')[1].alias('score'))
 
-        for column in existing_match_df.columns:
-            existing_match_df = existing_match_df.withColumnRenamed(column, 'match_' + column)
+#         for column in existing_match_df.columns:
+#             existing_match_df = existing_match_df.withColumnRenamed(column, 'match_' + column)
 
-        existing_match_df.show()
-        print('---------------count of existing is',existing_match_df.count())
-        existing_match_df.printSchema()
-        print('-----------------match------------------',existing_match_df.count())
-        delta_match_df.printSchema()
-        print('------------delta-----------------------',delta_match_df.count())
+#         existing_match_df.show()
+#         print('---------------count of existing is',existing_match_df.count())
+#         existing_match_df.printSchema()
+#         print('-----------------match------------------',existing_match_df.count())
+#         delta_match_df.printSchema()
+#         print('------------delta-----------------------',delta_match_df.count())
          
-        #  we have tpo add 3 and 4 here 
-        full_df = delta_match_df.join(existing_match_df, (delta_match_df["pair_id"] == existing_match_df["match_pair_id"]) & (delta_match_df["source_type"] == existing_match_df["match_source_type"]),how='full_outer')
-        full_df.cache()
-        full_df=full_df.withColumn('final_product_segment',when(isnull(col('product_Segment')) , col('match_product_segment')).otherwise(col('product_Segment')))
-        full_df=full_df.withColumn('final_pair_id',when(isnull(col('pair_id')) , col('match_pair_id')).otherwise(col('pair_id')))
-        full_df=full_df.withColumn('final_type',when(isnull(col('source_type')),col('match_source_type')).otherwise(col('source_type')))
-        full_df=full_df.withColumn('final_score',when(isnull(col('score')),col('match_score')).otherwise(col('score')))
-        full_df.printSchema()
-        print('------------ful',full_df.count())
-        full_df.show()
-        full_df.write.format("parquet").mode("overwrite").save('s3://mdp-ut.east1/aggregated/',header = 'true')
-        return full_df
+#         #  we have tpo add 3 and 4 here 
+#         full_df = delta_match_df.join(existing_match_df, (delta_match_df["pair_id"] == existing_match_df["match_pair_id"]) & (delta_match_df["source_type"] == existing_match_df["match_source_type"]),how='full_outer')
+#         full_df.cache()
+#         full_df=full_df.withColumn('final_product_segment',when(isnull(col('product_Segment')) , col('match_product_segment')).otherwise(col('product_Segment')))
+#         full_df=full_df.withColumn('final_pair_id',when(isnull(col('pair_id')) , col('match_pair_id')).otherwise(col('pair_id')))
+#         full_df=full_df.withColumn('final_type',when(isnull(col('source_type')),col('match_source_type')).otherwise(col('source_type')))
+#         full_df=full_df.withColumn('final_score',when(isnull(col('score')),col('match_score')).otherwise(col('score')))
+#         full_df.printSchema()
+#         print('------------ful',full_df.count())
+#         full_df.show()
+#         full_df.write.format("parquet").mode("overwrite").save('s3://mdp-ut.east1/aggregated/',header = 'true')
+#         return full_df
+    
+
+class Aggregator:
+    def __init__(self, glue_context, ml_match_suggestion, upc_match_suggestion, mpn_match_suggestion, transitive_match_suggestion, mdw) -> None:
+        self.ml_match_suggestion = ml_match_suggestion
+        self.upc_match_suggestion = upc_match_suggestion
+        self.mpn_match_suggestion = mpn_match_suggestion
+        self.transitive_match_suggestion = transitive_match_suggestion
+        self.mdw = mdw
+        self.glue_context = glue_context
+
+    def aggregate_match():
+        
+        
+        
+    # def aggregate(self,delta_match_df,existing_match_df):
+    #     existing_match_df.cache()
+    #     delta_match_df.cache()
+    #     existing_match_df.show()
+    #     delta_match_df.show()
+    #     print('---------------count of existing b4 explode is',existing_match_df.count())
+        
+    #     existing_match_df=existing_match_df.withColumn("type_score_list", explode(existing_match_df.type_score_list))
+    #     existing_match_df=existing_match_df.select(col('*'),col('type_score_list')[0].alias('source_type'),col('type_score_list')[1].alias('score'))
+
+    #     for column in existing_match_df.columns:
+    #         existing_match_df = existing_match_df.withColumnRenamed(column, 'match_' + column)
+
+    #     existing_match_df.show()
+    #     print('---------------count of existing is',existing_match_df.count())
+    #     existing_match_df.printSchema()
+    #     print('-----------------match------------------',existing_match_df.count())
+    #     delta_match_df.printSchema()
+    #     print('------------delta-----------------------',delta_match_df.count())
+         
+    #     #  we have tpo add 3 and 4 here 
+    #     full_df = delta_match_df.join(existing_match_df, (delta_match_df["pair_id"] == existing_match_df["match_pair_id"]) & (delta_match_df["source_type"] == existing_match_df["match_source_type"]),how='full_outer')
+    #     full_df.cache()
+    #     full_df=full_df.withColumn('final_product_segment',when(isnull(col('product_Segment')) , col('match_product_segment')).otherwise(col('product_Segment')))
+    #     full_df=full_df.withColumn('final_pair_id',when(isnull(col('pair_id')) , col('match_pair_id')).otherwise(col('pair_id')))
+    #     full_df=full_df.withColumn('final_type',when(isnull(col('source_type')),col('match_source_type')).otherwise(col('source_type')))
+    #     full_df=full_df.withColumn('final_score',when(isnull(col('score')),col('match_score')).otherwise(col('score')))
+    #     full_df.printSchema()
+    #     print('------------ful',full_df.count())
+    #     full_df.show()
+    #     full_df.write.format("parquet").mode("overwrite").save('s3://mdp-ut.east1/aggregated/',header = 'true')
+    #     return full_df
--- match_generator/src/data_reader/data_reader.py
+++ match_generator/src/data_reader/data_reader.py
@@ -2,7 +2,45 @@ from bungee_utils.spark_utils.db_reader.s3 import S3Reader
 from bungee_utils.spark_utils.db_reader.aurora import AuroraReader
 from bungee_utils.spark_utils.db_reader.athena import AthenaReader
 from pyspark.sql.types import DoubleType
+from pyspark.sql import DataFrame
+from pyspark.sql.types import *
+from pyspark.sql.session import SparkSession
 
+class DataFetcher:
+    def __init__(self, args, spark:SparkSession ) -> None:
+        self.args = args
+        self.spark = spark
+        empty_schema = StructType([])
+        # Create an empty DataFrame
+        self.empty_df = self.spark.createDataFrame([], schema=empty_schema)
+        pass
+    
+    def _fetch_upc_matches(self) -> DataFrame:
+        upc_match_suggestion = self.empty_df
+        return upc_match_suggestion
+    
+    def fetch_ml_matches(self) -> DataFrame:
+        ml_match_suggestion = self.empty_df
+        return ml_match_suggestion
+    
+    def fetch_transitive_matches(self) -> DataFrame:
+        transitive_match_suggestion = self.empty_df
+        return transitive_match_suggestion
+    
+    def fetch_mpn_matches(self) -> DataFrame:
+        mpn_match_suggestion = self.empty_df
+        return mpn_match_suggestion
+    
+    def fetch_mdw(self) -> DataFrame:
+        mdw = self.spark.sql("""SELECT * FROM (
+                                    SELECT * FROM {mdw_database}.{mdw_table} A
+                                    JOIN
+                                    SELECT * FROM {temp_match_database}.{temp_match_table} B
+                                    ON A.pair_id == B.pair_id
+                                )""")
+        
+        return mdw
+    
 class DataReader:
     def __init__(self,glue_context):
         self.glue_context=glue_context
