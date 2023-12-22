95b66b5060792968b2d8b8c9a0d2dd1235453081
borahkaushik <kaushik@bungeetech.com>
2023-12-22 19:34:55 +0530

    changes in fastlane column

--- aw_to_mw_synchronizer/aw_to_mw_synchronizer/transformer/bungee_audit_match_processor.py
+++ aw_to_mw_synchronizer/aw_to_mw_synchronizer/transformer/bungee_audit_match_processor.py
@@ -52,12 +52,24 @@ class BungeeAuditMatchProcessor:
                                     .withColumn(MATCH_WAREHOUSE.PAIR_ID.NAME, concat_ws("_", col("sku_uuid_a"), col("sku_uuid_b")))\
                                     .withColumn(MATCH_WAREHOUSE.AGGREGATED_SCORE.NAME, when( col("score") > 1, 1.0 ).otherwise( col("score") ) )\
                                     .withColumn(MATCH_WAREHOUSE.BUNGEE_AUDIT_STATUS.NAME, 
-                                                    when( col("answer").contains("exact"), lit(BUNGEE_AUDIT_STATUS.EXACT_MATCH))\
-                                                    .when( col("answer").contains("similar") | col("answer").contains("equivalent"), lit(BUNGEE_AUDIT_STATUS.SIMILAR_MATCH))\
+                                                    when( col("match").contains("exact"), lit(BUNGEE_AUDIT_STATUS.EXACT_MATCH))\
+                                                    .when( col("match").contains("similar") | col("answer").contains("equivalent"), lit(BUNGEE_AUDIT_STATUS.SIMILAR_MATCH))\
                                                     .otherwise( lit(BUNGEE_AUDIT_STATUS.NOT_MATCH) )
                                                 ) \
+                                    .withColumn(MATCH_WAREHOUSE.AGGREGATED_SCORE.NAME, to_date(col("score")) ) \
+                                    .withColumn("match_source", 
+                                                    when( col("model_used").contains("ml"), lit(MATCH_SOURCE.ML))\
+                                                    .when( col("model_used").contains("upc"), lit(MATCH_SOURCE.UPC))\
+                                                    .when( col("model_used").contains("mpn"), lit(MATCH_SOURCE.MPN))\
+                                                    .when( col("model_used").contains("manual"), lit(MATCH_SOURCE.MANUAL))\
+                                                    .when( col("model_used").contains("transitive"), lit(MATCH_SOURCE.TRANSITIVE))\
+                                                    .otherwise( lit(MATCH_SOURCE.LEGACY) )
+                                                ) \
+                                    .withColumn(MATCH_WAREHOUSE.MATCH_SOURCE_SCORE_MAP.NAME, create_map("match_source",MATCH_WAREHOUSE.AGGREGATED_SCORE.NAME)) \
                                     .withColumn(MATCH_WAREHOUSE.BUNGEE_AUDIT_DATE.NAME, to_date(col("match_date")) ) \
-                                    .withColumn(MATCH_WAREHOUSE.BUNGEE_AUDITOR.NAME, lit(None) ) \
+                                    .withColumn(MATCH_WAREHOUSE.BUNGEE_AUDITOR.NAME, lit(col("matcher")) ) \
+                                    .withColumn(MATCH_WAREHOUSE.MISC_INFO.NAME, lit(None)) \
+                                    .withColumn(MATCH_WAREHOUSE.SELLER_TYPE.NAME, lit(None)) \
                                     .withColumn(MATCH_WAREHOUSE.CLIENT_AUDIT_STATUS_L1.NAME, lit(None) ) \
                                     .withColumn(MATCH_WAREHOUSE.CLIENT_AUDITOR_L1.NAME, lit(None) ) \
                                     .withColumn(MATCH_WAREHOUSE.CLIENT_AUDIT_DATE_L1.NAME, lit(None) ) \
@@ -71,8 +83,6 @@ class BungeeAuditMatchProcessor:
                                     .withColumn(MATCH_WAREHOUSE.CREATED_BY.NAME, lit(None)) \
                                     .withColumn(MATCH_WAREHOUSE.UPDATED_DATE.NAME, lit(None)) \
                                     .withColumn(MATCH_WAREHOUSE.UPDATED_BY.NAME, lit(None)) \
-                                    .withColumn(MATCH_WAREHOUSE.MISC_INFO.NAME, lit(None)) \
-                                    .withColumn(MATCH_WAREHOUSE.SELLER_TYPE.NAME, lit(None)) \
                                     .withColumn("priority", lit(2))
         bungee_audit_matches = bungee_audit_matches.select(get_column_list(MATCH_WAREHOUSE))
         return bungee_audit_matches
